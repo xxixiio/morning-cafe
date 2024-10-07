078c20ab5141f835be2223e46daacecd9c3e56b8
hizer00 <{ID}+{username}@users.noreply.github.com>
2024-10-08 00:51:41 +0200

    deploy 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Navbar } from './sections/Navbar'
-import { HeroSection } from './sections/HeroSection'
-import { Features } from './sections/Features'
-import { FeaturedProduct } from './sections/FeaturedProduct'
-import { FeaturedCoffees } from './sections/FeaturedCoffees'
-import { MissionStatement } from './sections/MissionStatement'
-import { Blog } from './sections/Blog'
-import { StoreLocations } from './sections/StoreLocations'
-import { Footer } from './sections/Footer'
+import { Navbar } from "./sections/Navbar";
+import { HeroSection } from "./sections/HeroSection";
+import { Features } from "./sections/Features";
+import { FeaturedProduct } from "./sections/FeaturedProduct";
+import { FeaturedCoffees } from "./sections/FeaturedCoffees";
+import { MissionStatement } from "./sections/MissionStatement";
+import { Blog } from "./sections/Blog";
+import { StoreLocations } from "./sections/StoreLocations";
+import { Footer } from "./sections/Footer";
 
 export default function CoffeeShopPage() {
   return (
@@ -21,5 +21,5 @@ export default function CoffeeShopPage() {
       <StoreLocations />
       <Footer />
     </div>
-  )
+  );
 }
